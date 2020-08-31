068580f865489b6cf9f95dbf5481b61eb4c8f76e
James Allen <jamesallen74@gmail.com>
2020-08-30 23:37:17 -0400

    Init commit

---
+++ console-di-logging-config/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_di_logging_config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
